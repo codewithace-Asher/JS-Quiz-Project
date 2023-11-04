f0fba9727515a475436adbfab5b5f5b53cecdb22
Asher Tauri <117128017+codewithace-Asher@users.noreply.github.com>
2023-11-05 10:36:20 +1300

    Update questions.js
    questions created

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,57 @@
-// JavaScript Document
+let questions = [
+    {
+        numb: 1,
+        question: "Where is the Maori culture primarily rooted?",
+        answer: "C. New Zealand",
+        options: [
+            "A. Samoa",
+            "B. Hawaiki",
+            "C. New Zealand",
+            "D. Cook Islands"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the traditional Maori practice of tattooing called?",
+        answer: "A. Ta Moko",
+        options: [
+            "A. Ta Moko",
+            "B. Nomi",
+            "C. Tribal",
+            "D. Mataora"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which Maori term refers to the deep connection between the people and the land?",
+        answer: "A. Kaitiakitanga",
+        options: [
+            "A. Kaitiakitanga",
+            "B. Maori Locals",
+            "C. Whanau",
+            "D. Tangatawhenua"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the name of the Maori language?",
+        answer: "D. Te Reo Maori",
+        options: [
+            "A. Maori o te Reo",
+            "B. Te te Reo",
+            "C. Maori Reo",
+            "D. Te Reo Maori"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Who is the legendary Maori demigod?",
+        answer: "D. Maui",
+        options: [
+            "A. Taniwha",
+            "B. Hone Heke",
+            "C. Ranginui",
+            "D. Maui"
+        ]
+    }
+];
